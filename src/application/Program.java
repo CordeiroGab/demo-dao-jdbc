7d06439e98a09238b935d7a5bc2c27a6ba59ba8c
Gabriel Neves <GabrielCordeiroDev@outlook.com>
2025-06-16 18:02:00 -0300

    Seller entity

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,19 @@
 package application;
 
 import model.entity.Department;
+import model.entity.Seller;
+
+import java.util.Date;
 
 public class Program {
 
     public static void main(String[] args) {
 
         Department obj = new Department(1,"Books");
+
+        Seller seller = new Seller(21, "Bob", "Bob@gmail.com", new Date(), 3000.00,obj);
         System.out.println(obj);
+        System.out.println(seller);
     }
+
 }
